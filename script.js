51d26ed08d59f870a1df245fa5db222e77399b08
edonb <edonb@live.no>
2022-08-22 20:01:58 +0200

    kanban

--- script.js
+++ script.js
@@ -20,17 +20,13 @@ var settings = {
        var ticketCreated = tickets[i]["created_at"].slice(0, -10)
        var ticketStatus = tickets[i]["status"]
 
-        if (ticketStatus == "11") {
+       if (ticketStatus == "2") {
+        $('#backlog').append(  "<div class='task' draggable='true'><div class='task__tags'><span class='task__tag task__tag--tag1'>Prioritet: "+ ticketPriority +"</span><button class='task__options'><i class='fas fa-ellipsis-h'></i></button></div><p>" + ticketSubject +"</p><div class='task__stats'><span><time datetime='2021-11-24T20:00:00'><i class='fas fa-flag'></i>"+ticketCreated+"</time></span></div>");
+        } else if (ticketStatus == "11") {
             $('#readyboard').append(  "<div class='task' draggable='true'><div class='task__tags'><span class='task__tag task__tag--tag1'>Prioritet: "+ ticketPriority +"</span><button class='task__options'><i class='fas fa-ellipsis-h'></i></button></div><p>" + ticketSubject +"</p><div class='task__stats'><span><time datetime='2021-11-24T20:00:00'><i class='fas fa-flag'></i>"+ticketCreated+"</time></span></div>");
-        } else 
-        console.log("no")
-
-        if (ticketStatus == "10") {
+        } else if (ticketStatus == "10") {
             $('#inprogressboard').append(  "<div class='task' draggable='true'><div class='task__tags'><span class='task__tag task__tag--tag3'>Prioritet: "+ ticketPriority +"</span><button class='task__options'><i class='fas fa-ellipsis-h'></i></button></div><p>" + ticketSubject +"</p><div class='task__stats'><span><time datetime='2021-11-24T20:00:00'><i class='fas fa-flag'></i>"+ticketCreated+"</time></span></div>");
-        } else 
-        console.log("no")
-
-        if (ticketStatus == "8") {
+        } else if (ticketStatus == "8" || ticketStatus == "7") {
             $('#blockedboard').append(  "<div class='task' draggable='true'><div class='task__tags'><span class='task__tag task__tag--tag4'>Prioritet: "+ ticketPriority +"</span><button class='task__options'><i class='fas fa-ellipsis-h'></i></button></div><p>" + ticketSubject +"</p><div class='task__stats'><span><time datetime='2021-11-24T20:00:00'><i class='fas fa-flag'></i>"+ticketCreated+"</time></span></div>");
         } else 
         console.log("no")
