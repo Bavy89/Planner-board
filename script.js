0ef4e55c7f49b77b52c47b9474c58a63bd31ed21
edonb <edonb@live.no>
2022-08-22 19:17:18 +0200

    k
    k

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ var settings = {
 
     for(var i=0; i<tickets.length; i++){
         console.log(tickets)
-        $('#test').append(  "<div class='task' draggable='true'><div class='task__tags'><span class='task__tag task__tag--tag1'>Tag 1</span><button class='task__options'><i class='fas fa-ellipsis-h'></i></button></div><p>" + tickets[i]["subject"] +"</p><div class='task__stats'><span><time datetime='2021-11-24T20:00:00'><i class='fas fa-flag'></i>"+tickets[i]["created_at"].slice(0, -10)+"</time></span></div>");
+        $('#readyboard').append(  "<div class='task' draggable='true'><div class='task__tags'><span class='task__tag task__tag--tag1'>Tag 1</span><button class='task__options'><i class='fas fa-ellipsis-h'></i></button></div><p>" + tickets[i]["subject"] +"</p><div class='task__stats'><span><time datetime='2021-11-24T20:00:00'><i class='fas fa-flag'></i>"+tickets[i]["created_at"].slice(0, -10)+"</time></span></div>");
     }
       
   });
