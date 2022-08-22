3c010b553136154e1c594d42eff7087321c23f34
edonb <edonb@live.no>
2022-08-22 22:39:33 +0200

    test

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ var settings = {
        if (ticketStatus == "2" || ticketStatus == "9") {
         $('#backlog').append(  "<div class='task' data-toggle='modal' data-target='#myModal' onclick='previewTicket("+ticketId+")'><div class='task__tags'><span class='task__tag task__tag--tag1' >Prioritet: "+ ticketPriority +"</span><button class='task__options'><i class='fas fa-ellipsis-h'></i></button></div><p>" + ticketSubject +"</p><div class='task__stats'><span><time datetime='2021-11-24T20:00:00'><i class='fas fa-flag'></i>Created: "+ticketCreated+" || id: "+ticketId+"</time><input style='float:right;width:3.5rem;'disabled='true' value='"+backlogState+"'></span></div>");
         } else if (ticketStatus == "11" ) {
-            $('#readyboard').append(  "<div class='task' data-toggle='modal' data-target='#myModal onclick='previewTicket("+ticketId+")'><div class='task__tags'><span class='task__tag task__tag--tag1'>Prioritet: "+ ticketPriority +"</span><button class='task__options'><i class='fas fa-ellipsis-h'></i></button></div><p>" + ticketSubject +"</p><div class='task__stats'><span><time datetime='2021-11-24T20:00:00'><i class='fas fa-flag'></i>Created: "+ticketCreated+" || id: "+ticketId+"</time></span></div>");
+            $('#readyboard').append(  "<div class='task' data-toggle='modal' data-target='#myModal' onclick='previewTicket("+ticketId+")'><div class='task__tags'><span class='task__tag task__tag--tag1'>Prioritet: "+ ticketPriority +"</span><button class='task__options'><i class='fas fa-ellipsis-h'></i></button></div><p>" + ticketSubject +"</p><div class='task__stats'><span><time datetime='2021-11-24T20:00:00'><i class='fas fa-flag'></i>Created: "+ticketCreated+" || id: "+ticketId+"</time></span></div>");
         } else if (ticketStatus == "10") {
             $('#inprogressboard').append(  "<div class='task' data-toggle='modal' data-target='#myModal' onclick='previewTicket("+ticketId+")'><div class='task__tags'><span class='task__tag task__tag--tag3'>Prioritet: "+ ticketPriority +"</span><button class='task__options'><i class='fas fa-ellipsis-h'></i></button></div><p>" + ticketSubject +"</p><div class='task__stats'><span><time datetime='2021-11-24T20:00:00'><i class='fas fa-flag'></i>Created: "+ticketCreated+" || id: "+ticketId+"</time></span></div>");
         } else if (ticketStatus == "8" || ticketStatus == "7") {
@@ -87,6 +87,7 @@ function previewTicket(ticketId) {
       
       $.ajax(settings).done(function (response) {
         console.log(response)
+        document.getElementById("modaldesc").innerHTML = "Ticket description: "+response["description"];
 
     
     
