0f09b8cd48d1ecb353058852c256d3d7d16ede6f
edonb <edonb@live.no>
2022-08-22 15:27:29 +0200

    Test

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-
-    var headers = {
-      Authorization: "Basic aElLdE5uRnJyWk1TTUZKRjI1TzpY",
-      dataType: "application/json",
-    };
-    var options = { headers: headers };
-    var url = 'https://specialist-cc.freshdesk.com/api/v2/search/tickets?query="status:2"';
-    get(url, options).then(
-      function (data) {
-        console.log(data);
-      },
-      function (error) {
-        console.log(error);
-      }
-    );
+var settings = {
+    "url": "https://specialist-cc.freshdesk.com/api/v2/search/tickets?query=\"status:2\"",
+    "method": "GET",
+    "timeout": 0,
+    "headers": {
+      "Content-Type": "application/json",
+      "Authorization": "Basic aElLdE5uRnJyWk1TTUZKRjI1TzpY",
+      "Cookie": "_x_m=x_c; _x_w=6_1"
+    },
+  };
+  
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
