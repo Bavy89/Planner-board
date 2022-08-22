d0120b6903a65f48946339af2e200eff67bb152e
edonb <edonb@live.no>
2022-08-22 14:40:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append("Authorization", "Basic aElLdE5uRnJyWk1TTUZKRjI1TzpY");
+myHeaders.append("Cookie", "_x_m=x_c; _x_w=6_1");
+
+var raw = "";
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch('https://specialist-cc.freshdesk.com/api/v2/search/tickets?query="status:2"', requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
