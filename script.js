a3e6dd6b710c4b2f0b14434ab4f3d691ec6de869
edonb <edonb@live.no>
2022-08-22 19:16:01 +0200

    logic created

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var settings = {
-    "url": "https://specialist-cc.freshdesk.com/api/v2/search/tickets?query=\"status:2\"",
+    "url": "https://specialist-cc.freshdesk.com/api/v2/tickets",
     "method": "GET",
     "timeout": 0,
     "headers": {
@@ -11,15 +11,13 @@ var settings = {
   
   $.ajax(settings).done(function (response) {
     console.log(response)
-    const arr = response
-    const myJSON = JSON.stringify(arr);
-    console.log(myJSON)
-    document.getElementById("test").innerHTML = response;
+    tickets = response
 
-    for (var i = 0; i < response.length; i++) {
-        console.log(myStringArray[i]);
-        console.log("lol")
+    for(var i=0; i<tickets.length; i++){
+        console.log(tickets)
+        $('#test').append(  "<div class='task' draggable='true'><div class='task__tags'><span class='task__tag task__tag--tag1'>Tag 1</span><button class='task__options'><i class='fas fa-ellipsis-h'></i></button></div><p>" + tickets[i]["subject"] +"</p><div class='task__stats'><span><time datetime='2021-11-24T20:00:00'><i class='fas fa-flag'></i>"+tickets[i]["created_at"].slice(0, -10)+"</time></span></div>");
     }
+      
   });
 
-console.log("hei")
+
