74d775f1b1ffd7efba2c8db62b690ecbfb6c7490
edonb <edonb@live.no>
2022-08-22 15:03:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-
-var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
-myHeaders.append("Authorization", "Basic aElLdE5uRnJyWk1TTUZKRjI1TzpY");
-myHeaders.append("Cookie", "_x_m=x_c; _x_w=6_1");
-
-var raw = "";
-
-var requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
-};
-
-fetch('https://specialist-cc.freshdesk.com/api/v2/search/tickets?query="status:2"', requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+    var headers = {
+      Authorization: "Basic aElLdE5uRnJyWk1TTUZKRjI1TzpY",
+      dataType: "application/json",
+    };
+    var options = { headers: headers };
+    var url = 'https://specialist-cc.freshdesk.com/api/v2/search/tickets?query="status:2"';
+    client.request.get(url, options).then(
+      function (data) {
+        console.log(data);
+      },
+      function (error) {
+        console.log(error);
+      }
+    );
