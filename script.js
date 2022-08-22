3f8ba38cb822b6d98e5d9b4a55b4d2d0f2cd3263
edonb <edonb@live.no>
2022-08-22 15:06:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-    client = client_
+
     var headers = {
       Authorization: "Basic aElLdE5uRnJyWk1TTUZKRjI1TzpY",
       dataType: "application/json",
     };
     var options = { headers: headers };
     var url = 'https://specialist-cc.freshdesk.com/api/v2/search/tickets?query="status:2"';
-    client.request.get(url, options).then(
+    get(url, options).then(
       function (data) {
         console.log(data);
       },
