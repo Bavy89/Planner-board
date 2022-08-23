2f42eabc464366df5ec67d55296a83fe64b98670
edonb <edonb@live.no>
2022-08-23 18:14:46 +0200

    all

--- script.js
+++ script.js
@@ -20,14 +20,14 @@ var settings = {
        var ticketCreated = tickets[i]["created_at"].slice(0, -10)
        var ticketStatus = tickets[i]["status"]
        var ticketId = tickets[i]["id"]
-        backlogState = "New"
+        backlogState = '<img style="position:absolute;right:0px;bottom:0px;" src="img/new.png" alt="new">'
 
         if (ticketStatus == "9") {
-            backlogState = "Backlog"
+            backlogState = '<img style="position:absolute;right:0px;bottom:0px;" src="img/backlog.png" alt="Backlog">'
         }
 
        if (ticketStatus == "2" || ticketStatus == "9") {
-        $('#backlog').append(  "<div class='task' data-toggle='modal' data-target='#myModal' onclick='previewTicket("+ticketId+")'><div class='task__tags'><span class='task__tag task__tag--tag1' >Prioritet: "+ ticketPriority +"</span><button class='task__options'><i class='fas fa-ellipsis-h'></i></button></div><p>" + ticketSubject +"</p><div class='task__stats'><span><time datetime='2021-11-24T20:00:00'><i class='fas fa-flag'></i>Created: "+ticketCreated+" || id: "+ticketId+"</time><input style='float:right;width:3.5rem;'disabled='true' value='"+backlogState+"'></span></div>");
+        $('#backlog').append(  "<div class='task' data-toggle='modal' data-target='#myModal' onclick='previewTicket("+ticketId+")'><div class='task__tags'><span class='task__tag task__tag--tag1' >Prioritet: "+ ticketPriority +"</span><button class='task__options'><i class='fas fa-ellipsis-h'></i></button></div><p>" + ticketSubject +"</p><div class='task__stats'><span><time datetime='2021-11-24T20:00:00'><i class='fas fa-flag'></i>Created: "+ticketCreated+" || id: "+ticketId+"</time>"+backlogState+"</span></div>");
         } else if (ticketStatus == "11" ) {
             $('#readyboard').append(  "<div class='task' data-toggle='modal' data-target='#myModal' onclick='previewTicket("+ticketId+")'><div class='task__tags'><span class='task__tag task__tag--tag1'>Prioritet: "+ ticketPriority +"</span><button class='task__options'><i class='fas fa-ellipsis-h'></i></button></div><p>" + ticketSubject +"</p><div class='task__stats'><span><time datetime='2021-11-24T20:00:00'><i class='fas fa-flag'></i>Created: "+ticketCreated+" || id: "+ticketId+"</time></span></div>");
         } else if (ticketStatus == "10") {
@@ -35,43 +35,14 @@ var settings = {
         } else if (ticketStatus == "8" || ticketStatus == "7") {
             $('#blockedboard').append(  "<div class='task' data-toggle='modal' data-target='#myModal' onclick='previewTicket("+ticketId+")'><div class='task__tags'><span class='task__tag task__tag--tag4'>Prioritet: "+ ticketPriority +"</span><button class='task__options'><i class='fas fa-ellipsis-h'></i></button></div><p>" + ticketSubject +"</p><div class='task__stats'><span><time datetime='2021-11-24T20:00:00'><i class='fas fa-flag'></i>Created: "+ticketCreated+"|| id: "+ticketId+"</time></span></div>");
         } else if (ticketStatus == "4") {
-            $('#doneboard').append(  "<div class='task' data-toggle='modal' data-target='#myModal' onclick='previewTicket("+ticketId+")'><div class='task__tags'><span class='task__tag task__tag--tag4'>Prioritet: "+ ticketPriority +"</span><button class='task__options'><i class='fas fa-ellipsis-h'></i></button></div><p>" + ticketSubject +"</p><div class='task__stats'><span><time datetime='2021-11-24T20:00:00'><i class='fas fa-flag'></i>Created: "+ticketCreated+" || id: "+ticketId+"</time></span></div>");
+            $('#doneboard').append(  "<div class='task' data-toggle='modal' data-target='#myModal'  onclick='previewTicket("+ticketId+")'><div class='task__tags'><span class='task__tag task__tag--tag4'>Prioritet: "+ ticketPriority +"</span><button class='task__options'><i class='fas fa-ellipsis-h'></i></button></div><p>" + ticketSubject +"</p><div class='task__stats'><span><time datetime='2021-11-24T20:00:00'><i class='fas fa-flag'></i>Created: "+ticketCreated+" || id: "+ticketId+"</time></span></div>");
         }
-        console.log("no")
     }
 
 
   });
 
 
-function testfunc() {
-console.log("hei")
-}
-
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
 
 function previewTicket(ticketId) {
     var settings = {
@@ -87,7 +58,8 @@ function previewTicket(ticketId) {
       
       $.ajax(settings).done(function (response) {
         console.log(response)
-        document.getElementById("modaldesc").innerHTML = "<h3>"+response["subject"]+"</h3><hr>Ticket description: "+response["description"];
+        document.getElementById("modaldesc").innerHTML = "<h4>"+response["subject"]+"</h4> <p>"+response["description"]+"</p>";
+        document.getElementById("taskId").innerHTML = 'Ticket: <a href="https://specialist-cc.freshdesk.com/a/tickets/'+ticketId+'"target="blank">'+ticketId+'</a>';
 
     
     
