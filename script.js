b01e9a38f490a0ef4eace4a249e84015291e72ea
edonb <edonb@live.no>
2022-08-23 23:53:51 +0200

    kanban

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var settings = {
-    "url": "https://specialist-cc.freshdesk.com/api/v2/tickets?per_page=100&page=1",
+    "url": 'https://specialist-cc.freshdesk.com/api/v2/tickets?per_page=100&page=1',
     "method": "GET",
     "timeout": 0,
     "headers": {
@@ -10,7 +10,7 @@ var settings = {
   };
   
   $.ajax(settings).done(function (response) {
-    console.log(response)
+console.log(response)
     tickets = response
 
     for(var i=0; i<tickets.length; i++){
@@ -19,20 +19,28 @@ var settings = {
        var ticketCreated = tickets[i]["created_at"].slice(0, -10)
        var ticketStatus = tickets[i]["status"]
        var ticketId = tickets[i]["id"]
-        backlogState = '<img style="position:absolute;right:0px;bottom:0px;" src="img/new.png" alt="new">'
-
+        backlogState = 'New'
+        console.log(tickets[i]["status"])
+        console.log(tickets[i]["id"])
+        console.log("hei")
         if (ticketStatus == "9") {
-            backlogState = '<img style="position:absolute;right:0px;bottom:0px;" src="img/backlog.png" alt="Backlog">'
+            backlogState = 'Backlog'
+        } else if (ticketStatus == "3") {
+          backlogState = 'Pending'
+        } else if (ticketStatus == "7") {
+          backlogState = 'Thirdparty'
+        } else if (ticketStatus == "8") {
+          backlogState = 'Blocked'
         }
 
        if (ticketStatus == "2" || ticketStatus == "9") {
-        $('#backlog').append(  "<div class='task' data-toggle='modal' data-target='#myModal' onclick='previewTicket("+ticketId+")'><div class='task__tags'><span class='task__tag task__tag--tag1' >Prioritet: "+ ticketPriority +"</span><button class='task__options'><i class='fas fa-ellipsis-h'></i></button></div><p>" + ticketSubject +"</p><div class='task__stats'><span><time datetime='2021-11-24T20:00:00'><i class='fas fa-flag'></i>Created: "+ticketCreated+" || id: "+ticketId+"</time>"+backlogState+"</span></div>");
+        $('#backlog').append(  "<div class='task' data-toggle='modal' data-target='#myModal' onclick='previewTicket("+ticketId+")'><div class='task__tags'><span class='task__tag task__tag--tag1' >Prioritet: "+ ticketPriority +"</span><button class='task__options'><i class='fas fa-ellipsis-h'></i></button></div><p>" + ticketSubject +"</p><div class='task__stats'><span><time datetime='2021-11-24T20:00:00'><i class='fas fa-flag'></i>Created: "+ticketCreated+" || id: "+ticketId+"</time><small style='position: absolute; bottom: 0; right: 0; width: 100px; text-align:right;''>"+backlogState+"</small></span></div>");
         } else if (ticketStatus == "11" ) {
             $('#readyboard').append(  "<div class='task' data-toggle='modal' data-target='#myModal' onclick='previewTicket("+ticketId+")'><div class='task__tags'><span class='task__tag task__tag--tag1'>Prioritet: "+ ticketPriority +"</span><button class='task__options'><i class='fas fa-ellipsis-h'></i></button></div><p>" + ticketSubject +"</p><div class='task__stats'><span><time datetime='2021-11-24T20:00:00'><i class='fas fa-flag'></i>Created: "+ticketCreated+" || id: "+ticketId+"</time></span></div>");
         } else if (ticketStatus == "10") {
             $('#inprogressboard').append(  "<div class='task' data-toggle='modal' data-target='#myModal' onclick='previewTicket("+ticketId+")'><div class='task__tags'><span class='task__tag task__tag--tag3'>Prioritet: "+ ticketPriority +"</span><button class='task__options'><i class='fas fa-ellipsis-h'></i></button></div><p>" + ticketSubject +"</p><div class='task__stats'><span><time datetime='2021-11-24T20:00:00'><i class='fas fa-flag'></i>Created: "+ticketCreated+" || id: "+ticketId+"</time></span></div>");
-        } else if (ticketStatus == "8" || ticketStatus == "7") {
-            $('#blockedboard').append(  "<div class='task' data-toggle='modal' data-target='#myModal' onclick='previewTicket("+ticketId+")'><div class='task__tags'><span class='task__tag task__tag--tag4'>Prioritet: "+ ticketPriority +"</span><button class='task__options'><i class='fas fa-ellipsis-h'></i></button></div><p>" + ticketSubject +"</p><div class='task__stats'><span><time datetime='2021-11-24T20:00:00'><i class='fas fa-flag'></i>Created: "+ticketCreated+"|| id: "+ticketId+"</time></span></div>");
+        } else if (ticketStatus == "8" || ticketStatus == "7" || ticketStatus == "3") {
+            $('#blockedboard').append(  "<div class='task' data-toggle='modal' data-target='#myModal' onclick='previewTicket("+ticketId+")'><div class='task__tags'><span class='task__tag task__tag--tag4'>Prioritet: "+ ticketPriority +"</span><button class='task__options'><i class='fas fa-ellipsis-h'></i></button></div><p>" + ticketSubject +"</p><div class='task__stats'><span><time datetime='2021-11-24T20:00:00'><i class='fas fa-flag'></i>Created: "+ticketCreated+"|| id: "+ticketId+"</time><small style='position: absolute; bottom: 0; right: 0; width: 100px; text-align:right;''>"+backlogState+"</small></span></div>");
         } else if (ticketStatus == "4") {
             $('#doneboard').append(  "<div class='task' data-toggle='modal' data-target='#myModal'  onclick='previewTicket("+ticketId+")'><div class='task__tags'><span class='task__tag task__tag--tag4'>Prioritet: "+ ticketPriority +"</span><button class='task__options'><i class='fas fa-ellipsis-h'></i></button></div><p>" + ticketSubject +"</p><div class='task__stats'><span><time datetime='2021-11-24T20:00:00'><i class='fas fa-flag'></i>Created: "+ticketCreated+" || id: "+ticketId+"</time></span></div>");
         }
@@ -58,11 +66,11 @@ function previewTicket(ticketId) {
       $(this).removeData('commentsdesc');
       $.ajax(settings).done(function (response) {
         console.log(response)
-        document.getElementById("modaldesc").innerHTML = "<h4>"+response["subject"]+"</h4> <p>"+response["description"]+"</p>";
-        document.getElementById("taskId").innerHTML = 'Ticket: <a href="https://specialist-cc.freshdesk.com/a/tickets/'+ticketId+'"target="blank">'+ticketId+'</a>';
+        document.getElementById("modaldesc").innerHTML = `<div class='alert alert-info' role='alert'><h4>${response["subject"]}</h4><small>${response["created_at"]}</small><hr> <p>${response["description"]} || ${response["custom_fields"]["cf_type2"]}</p></div>`;
+        document.getElementById("taskId").innerHTML = `Ticket: <a href="https://specialist-cc.freshdesk.com/a/tickets/${ticketId}" target="blank">${ticketId}</a>`;
         console.log(response["conversations"])
         response["conversations"].map(element => {
-          $('#commentsdesc').replaceChildren("<div class='alert alert-warning' role='alert'"+element["body"]+"</div>");
+          $('#commentsdesc').append(`<div class='alert alert-warning' role='alert'><h4>Kommentar</h4><small>${element["created_at"]}</small> <hr>${   element["body"]}</div>`);
 
         });
       
