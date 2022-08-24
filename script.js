c6bffe96d26a842f01375fc61f874be5419a79dd
edonb <edonb@live.no>
2022-08-24 22:42:07 +0200

    kanban

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ loadBoard();
 
 function loadBoard(){
   var settings = {
-    "url": `${baseUrl}api/v2/search/tickets?query="status:2%20OR%20status:9%20OR%20status:11%20OR%20status:10%20OR%20status:8%20OR%20status:7%20OR%20status:3%20OR%20status:4%20AND%20created_at:%272022-01-01%27"&page=${callCount}`,
+    "url": `${baseUrl}api/v2/search/tickets?query="status:2%20OR%20status:9%20OR%20status:11%20OR%20status:10%20OR%20status:8%20OR%20status:7%20OR%20status:3%20OR%20status:4%20OR%20status:12%20AND%20created_at:%272022-01-01%27"&page=${callCount}`,
     "method": "GET",
     "timeout": 0,
     "headers": {
@@ -16,6 +16,7 @@ function loadBoard(){
   };
 
   $.ajax(settings).done(function (response) {
+    console.log(response)
     tickets = response["results"]
     mainResponse = response
     for (var i = 0; i < tickets.length; i++) {
@@ -41,17 +42,17 @@ function loadBoard(){
 
 
       if (ticketStatus == "9") {
-        backlogState = 'Backlog'
+        backlogState = '<span class="task__tag5 task__tag5--tag5">Backlog</span>'
       } else if (ticketStatus == "3") {
-        backlogState = 'Pending'
+        backlogState = '<span class="task__tag5 task__tag5--tag5">Pending</span>'
       } else if (ticketStatus == "7") {
-        backlogState = 'Thirdparty'
+        backlogState = '<span class="task__tag5 task__tag5--tag5">Thirdparty</span>'
       } else if (ticketStatus == "8") {
-        backlogState = 'Blocked'
+        backlogState = '<span class="task__tag5 task__tag5--tag5">Blocked</span>'
       } else if (ticketStatus == "2") {
-        backlogState = 'Ny'
-      } else if (ticketStatus == "4") {
-        backlogState = 'Resolved'
+        backlogState = '<span class="task__tag5 task__tag5--tag5">Ny</span>'
+      } else if (ticketStatus == "12") {
+        backlogState = 'Done'
       } else {
           var backlogState = "";
         }
@@ -66,17 +67,18 @@ function loadBoard(){
         var taskType = "#inprogressboard";
       } else if (ticketStatus == "8" || ticketStatus == "7" || ticketStatus == "3") {
         var taskType = "#blockedboard";
-      } else if (ticketStatus == "4") {
+      } else if (ticketStatus == "12") {
         var taskType = "#doneboard";
       } else {
         var taskType = "null";
       }
 
 
-      $(taskType).append(`<div class='task' data-toggle='modal' data-target='#myModal'  onclick='previewTicket("${ticketId}")'><div class='task__tags'>${ticketPriority}<button class='task__options'><i class='fas fa-ellipsis-h'></i></button></div><p>${ticketSubject}</p><div class='task__stats'><span><time datetime='2021-11-24T20:00:00'><i class='fas fa-flag'></i>Opprettet: ${ticketCreated} || id: ${ticketId}</time><small style='position: absolute; bottom: 0; right: 0; width: 100px; text-align:right;'><b>${backlogState}</b></small></span></div>`)
+      $(taskType).append(`<div class='task' data-toggle='modal' data-target='#myModal'  onclick='previewTicket("${ticketId}")'><div class='task__tags'>${ticketPriority}<button class='task__options'><i class='fas fa-ellipsis-h'></i></button></div><p>${ticketSubject}</p><div class='task__stats'><span><time datetime='2021-11-24T20:00:00'><i class='fas fa-flag'></i>Opprettet: ${ticketCreated} || id: ${ticketId}</time><small style='position: absolute; bottom: 0; right: 0; width: 100px; text-align:right;'>${backlogState}</small></div>`)
     }
-
+  checkBoard();
   });
+
 }
 
 
@@ -144,11 +146,7 @@ function sendNote(ticketId) {
 
     
 
-window.onscroll = function() {
- if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
-    checkBoard();
- }
-}
+
 
 function checkBoard() {
   if (mainResponse["results"].length == 0) {
